26abf943d34bc5cc7986d7044028374ceb90c6c1
Priyansh Gupta <priyanshgupta1227@gmail.com>
2023-09-21 17:38:54 +0530

    first commit

--- anuu.java
+++ anuu.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class anuu{
     public static void main(String args[]){
-        Scanner sc  = new Scanner(System.in);
-        char[] a = sc.next().toCharArray();
+        Scanner Sc  = new Scanner(System.in);
+        char[] a = Sc.next().toCharArray();
         for(int i=0;i<a.length;i++){
             if(a[i]>'4'){
                 a[i] = (char) ('9'-a[i]+'0');
